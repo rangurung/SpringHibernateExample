3b0baa0da80bf6eb12da4dfcc24c0b202db1128a
Ran Gurung <rangurung.rsr@gmail.com>
2016-09-19 13:58:41 -0400

    Spring Hibernate example

---
+++ src/main/java/com/springhibernate/dao/EmployeeDao.java
@@ -0,0 +1,18 @@
+package com.springhibernate.dao;
+
+import java.util.List;
+
+import com.springhibernate.model.Employee;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+	
+	void saveEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+	
+	List<Employee> findAllEmployees();
+	
+	Employee findEmployeeBySsn(String ssn);
+}
